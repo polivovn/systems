d01784cee35628e2f15bd44c6b6d311ba94770a3
Polina <pooina123@gmail.com>
2023-11-26 18:59:23 +0500

    first commit

---
+++ src/main/java/Epic.java
@@ -0,0 +1,22 @@
+public class Epic extends Task {
+    private String[] subtasks;
+
+    public Epic(int id, String[] subtasks) {
+        super(id);
+        this.subtasks = subtasks;
+    }
+
+    public String[] getSubtasks() {
+        return subtasks;
+    }
+
+    public boolean matches(String query) {
+        for (int i = 0; i < subtasks.length; i++) {
+            if (subtasks[i].contains(query)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
